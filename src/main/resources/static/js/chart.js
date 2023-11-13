01fa77e4b24937591520414dc59ce1e84061e929
orogno24 <orogno24@naver.com>
2023-11-13 12:34:38 +0900

    1113

--- src/main/resources/static/js/chart.js
+++ src/main/resources/static/js/chart.js
@@ -169,4 +169,42 @@ new Chart(ctx, {
     }
 });
 
+Chart(document.getElementById('myChart'), {
+    type: 'bar',
+    data: {
+        labels: ["January", "February", "March", "April", 'May', 'June', 'August', 'September'],
+        datasets: [{
+            label: "Lost",
+            data: [45, 25, 40, 20, 60, 20, 35, 25],
+            backgroundColor: "#0d6efd",
+            borderColor: 'transparent',
+            borderWidth: 2.5,
+            barPercentage: 0.4,
+        }, {
+            label: "Succes",
+            startAngle: 2,
+            data: [20, 40, 20, 50, 25, 40, 25, 10],
+            backgroundColor: "#dc3545",
+            borderColor: 'transparent',
+            borderWidth: 2.5,
+            barPercentage: 0.4,
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                gridLines: {},
+                ticks: {
+                    stepSize: 15,
+                },
+            }],
+            xAxes: [{
+                gridLines: {
+                    display: false,
+                }
+            }]
+        }
+    }
+})
+
 
